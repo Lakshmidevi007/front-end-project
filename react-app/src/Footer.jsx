4602464d3364d7b22486ccac1befb8184a6de045
Lakshmidevi007 <velmuruganseeman5@gmail.com>
2025-09-14 19:49:46 -0700

    first commit

--- react-app/src/Footer.jsx
+++ react-app/src/Footer.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
-import img from "./assets/spotify.svg"
-import img1 from "./assets/facebook.svg"
-import img2 from "./assets/instagram.png"
-import img3 from "./assets/twitter.png"
-
 
 function Footer() {
   return (
-    <div className='social-links-icons'>
-   
-   
+    <div className='footer'>
+       <h4>Discover</h4>
+      <p>Title</p>
+      <p>Authors</p>
+      <p>Subject</p>
+      <p>Language</p>
+      <div className='footer1'>
+        <h4>open library</h4>  
+        <p>blog</p>
+        
+<p>Vision</p>
+<p>Volunteer</p>
 
+<p>Careers</p>
+
+        
+        
       
-      <img src={img} alt="" width={30} />
-      <img src={img1} alt="" width={30}/>
-      <img src={img2} alt="" width={30} />
-      <img src={img3} alt='' width={30}/>
-      </div>
-     
-   
+      
+      
+    </div>
+
+    
+
+    </div>
   )
 }
 
